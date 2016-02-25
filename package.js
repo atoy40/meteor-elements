42dc2cc94689562ad7f65ea4462cb54c3372310e
Anthony Hinsinger <anthony.hinsinger@univ-pau.fr>
2016-02-25 10:22:30 +0100

    Updated bower package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'atoy40:meteor-elements',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'Integrate Meteor API with Polymer and web elements',
   git: 'https://github.com/atoy40/meteor-elements.git',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.imply(['tracker','mongo','session','ejson'],'client');
-  api.imply('mquandalle:bower@1.0.0');
+  api.imply('mquandalle:bower@1.5.0');
   api.imply('differential:vulcanize@3.0.0');
   api.addFiles('meteor-elements.js');
 });
